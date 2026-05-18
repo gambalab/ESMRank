0d7fe11b907f7f1514152cd3741d9c62d12a95c7
Riccardo Arnese <42966046+j3rk0@users.noreply.github.com>
2026-05-18 15:28:26 +0200

    Update profile.py

--- scripts/profile.py
+++ scripts/profile.py
@@ -1,3 +1,9 @@
 # TODO: if aggregate is true and fasta mode is true, aggregate position wise if alanines no
 # cast to position wise and apply sliding window
 # save as tsv position x score
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+from tqdm import tqdm
+import os
+import numpy as np
